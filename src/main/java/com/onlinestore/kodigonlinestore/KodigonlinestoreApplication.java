a8e5e00325c332254fb2b03a0af2cd89c8e560dd
Sorraimi Rivas <sorraimi.rivas@hotmail.com>
2022-02-03 15:50:03 -0400

    first commit

---
+++ src/main/java/com/onlinestore/kodigonlinestore/KodigonlinestoreApplication.java
@@ -0,0 +1,13 @@
+package com.onlinestore.kodigonlinestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KodigonlinestoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KodigonlinestoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/onlinestore/kodigonlinestore/KodigonlinestoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onlinestore.kodigonlinestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KodigonlinestoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
