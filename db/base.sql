cbd5cbccec053d70f4c241a599418d1fe7909b72
Sorraimi Rivas <sorraimi.rivas@hotmail.com>
2022-02-05 19:13:50 -0400

    added db to the project files.

---
+++ db/base.sql
@@ -0,0 +1,74 @@
+create database bookstore;
+
+create table customer(
+    id_customer int(11) auto_increment primary key not null,
+    client_name varchar(50),
+    lastname varchar(50),
+    adress varchar(100),
+    email varchar(50),
+    telephone int(15)
+);
+
+create table shopping_cart (
+    id_shopping_cart int(11) auto_increment primary key not null,
+    total float,
+    id_customer int(11),
+    foreign key (id_customer) references customer(id_customer)
+);
+
+create table book(
+    id_book int(11) auto_increment primary key  not null,
+    title varchar(50),
+    price_book float,
+    img longblob,
+    category varchar(10),
+    author varchar(50),
+    editorial varchar(20)
+);
+
+create table item_cart(
+    id_item_cart int(11) auto_increment primary key not null,
+    quantity int(11),
+    subtotal float,
+    id_book int(11),
+    id_shopping_cart int(11),
+    foreign key (id_book) references book(id_book),
+    foreign key (id_shopping_cart) references shopping_cart( id_shopping_cart)
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main/java/com/onlinestore/kodigonlinestore/main.java
+++ src/main/java/com/onlinestore/kodigonlinestore/main.java
@@ -1,4 +1,6 @@
-package com.onlinestore.kodigonlinestore.ConnectionDB;
+package com.onlinestore.kodigonlinestore;
+
+import com.onlinestore.kodigonlinestore.ConnectionDB.ConnectionDB;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -12,14 +14,14 @@ public class main {
 
 try{
     //crea objeto statement
-    Statement miStatement = conn.createStatement();
+    Statement myStatement = conn.createStatement();
     //Ejecutar query
-    ResultSet rs = miStatement.executeQuery("SELECT * FROM CLIENT");
+    ResultSet rs = myStatement.executeQuery("SELECT * FROM CUSTOMER");
     // recorrer el result set
     while(rs.next()){
         //nombre de la columna
-        System.out.println(rs.getString("id_client")+" "+rs.getString("client_name")
-        +" "+rs.getString("lastname")+" "+rs.getString("adress")+" "+
+        System.out.println(rs.getString("id_customer")+" "+rs.getString("name")
+        +" "+rs.getString("lastname")+" "+rs.getString("address")+" "+
                 rs.getString("email")+ " "+rs.getString("telephone"));
     }
 } catch (SQLException throwables) {
