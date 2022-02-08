b7d74788b169abbbfd72051447c7663bc5622bd3
emanuelprisker <emanuelprisker@gmail.com>
2022-02-08 17:41:07 -0300

    Se agrega apartado de Mailing

---
+++ src/main/java/com/onlinestore/kodigonlinestore/controller/Mail.java
@@ -0,0 +1,18 @@
+package com.onlinestore.kodigonlinestore.controller;
+
+import com.onlinestore.kodigonlinestore.model.Item;
+import java.util.Scanner;
+
+public class Mail {
+    public static void SendBillTo(Item item)throws Exception{
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a Mail to send the Bill");
+        String Mail = sc.nextLine();
+        StringBuilder bodyText = new StringBuilder();
+
+        bodyText.append("Book Title: "+ item.getTitle().toUpperCase() +"\n");
+        bodyText.append("Price: "+ item.getItemPrice().toUpperCase() +"\n");
+
+        MailSender.sendMail(Mail,bodyText.toString());
+    }
+}
---
+++ src/main/java/com/onlinestore/kodigonlinestore/controller/MailSender.java
@@ -0,0 +1,55 @@
+package com.onlinestore.kodigonlinestore.controller;
+
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+import java.util.Properties;
+
+public class MailSender {
+    private static Object Session;
+
+    public static  void sendMail(String receiver, String bodyText) throws Exception {
+        System.out.println("Preparando mail");
+        Properties properties = new Properties();
+
+        properties.put("mail.smtp.auth", "true");
+        properties.put("mail.smtp.starttls.enable", "true");
+        properties.put("mail.smtp.host", "smtp.gmail.com");
+        properties.put("mail.smtp.port", 587);
+
+        String myAccountEmail = "projectcinema321@gmail.com";
+        String password = "admin123!";
+
+        Session session = javax.mail.Session.getInstance(properties, new Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(myAccountEmail, password);
+            }
+        });
+        try {
+            MimeMessage message = new MimeMessage(session);
+            message.setFrom(new InternetAddress(myAccountEmail));
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(receiver));
+            message.setSubject("Reporte");
+
+            Multipart emailContent = new MimeMultipart();
+
+            MimeBodyPart textBodyPart = new MimeBodyPart();
+            textBodyPart.setText(bodyText);
+
+            MimeBodyPart txtAttachment = new MimeBodyPart();
+            txtAttachment.attachFile("Data\\Report.txt");
+
+            emailContent.addBodyPart(textBodyPart);
+            emailContent.addBodyPart(txtAttachment);
+            message.setContent(emailContent);
+
+            Transport.send(message);
+
+            System.out.println("ENVIADO");
+        }catch (MessagingException mex){
+            mex.printStackTrace();
+        }
+    }
+}
