4964244a423266fedb01ee4d0e41ac93774f689a
Sorraimi Rivas <sorraimi.rivas@hotmail.com>
2022-02-07 23:29:45 -0400

    added sign in box with some style

---
+++ src/main/resources/static/webapp/reactjs/src/components/accountForm/index.jsx
@@ -0,0 +1,79 @@
+import React from "react";
+import styled from "styled-components";
+
+const BoxContainer = styled.div`
+  width: 300px;
+  min-height: 550px;
+  display: flex;
+  flex-direction: column;
+  border-radius: 19px;
+  background-color: #fff;
+  box-shadow: 0 0 2px rgba(15, 15, 15, 0.28);
+  position: relative;
+  overflow: hidden;
+`;
+
+const TopContainer = styled.div`
+  width: 100%;
+  height: 250px;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  padding: 0 1.8em;
+  padding-bottom: 5em;
+`;
+
+const BackDrop = styled.div`
+  width: 160%;
+  height: 600px;
+  position: absolute;
+  display: flex;
+  flex-direction: column;
+  border-radius: 50%;
+  transform: rotate(45deg);
+  top: -290px;
+  left: -70px;
+  background: rgb(2, 0, 36);
+  background: linear-gradient(
+    90deg,
+    rgba(2, 0, 36, 1) 0%,
+    rgba(9, 9, 121, 1) 27%,
+    rgba(0, 212, 255, 1) 100%
+  );
+`;
+
+const HeaderContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+const HeaderText = styled.h2`
+  font-size: 30px;
+  font-weight: 600;
+  line-height: 1.24;
+  color: #fff;
+  z-index: 10;
+  margin: 0;
+`;
+
+const SmallText = styled.h5`
+  color: #fff;
+  font-weight: 500;
+  font-size: 14px;
+  z-index: 10;
+  margin: 0;
+  margin-top: 10px;
+`;
+
+export function AccountForm(props) {
+  return (
+    <BoxContainer>
+      <TopContainer>
+        <BackDrop />
+        <HeaderText>Welcome to the Kodig Online Store</HeaderText>
+        <SmallText>Please sign in to continue.</SmallText>
+      </TopContainer>
+    </BoxContainer>
+  );
+}
